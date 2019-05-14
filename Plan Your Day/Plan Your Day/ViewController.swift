a8feb9243ef7e28d1a633488082350e18745276d
Sandie Moore <smoore@rmn.com>
2019-05-14 11:47:56 -0700

    Add launch screen and date/time panel with timer

--- Plan Your Day/Plan Your Day/ViewController.swift
+++ Plan Your Day/Plan Your Day/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var timer = Timer()
+    var date = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        timeLabel.text = getTime(date: date)
+        dateLabel.text = getDate(date: date)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.tick) , userInfo: nil, repeats: true)
+
+    }
+    
+    func getTime(date : Date) -> String {
+        let dateFormatter : DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        return dateFormatter.string(from : date)
+    }
+    
+    func getDate(date : Date) -> String {
+        let dateFormatter : DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
+        return dateFormatter.string(from : date)
+    }
+    
+    @objc func tick() {
+        date = Date();
+        timeLabel.text = getTime(date: date)
+    }
 
 }
 
