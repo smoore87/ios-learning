ac8c11a79f65945dc2adc7a6ff4dcbde5572f179
Sandie Moore <smoore@rmn.com>
2019-05-14 12:06:14 -0700

    Add view for Settings

--- Plan Your Day/Plan Your Day/ViewController.swift
+++ Plan Your Day/Plan Your Day/ViewController.swift
@@ -11,29 +11,42 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var welcomeLabel: UILabel!
     
     var timer = Timer()
     var date = Date()
+    let dateFormatter = DateFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setWelcomeLabel()
         timeLabel.text = getTime(date: date)
         dateLabel.text = getDate(date: date)
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.tick) , userInfo: nil, repeats: true)
-
     }
     
     func getTime(date : Date) -> String {
-        let dateFormatter : DateFormatter = DateFormatter()
         dateFormatter.dateFormat = "h:mm a"
         return dateFormatter.string(from : date)
     }
     
     func getDate(date : Date) -> String {
-        let dateFormatter : DateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
         return dateFormatter.string(from : date)
+    }
+    
+    func setWelcomeLabel() {
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        if(hour < 12){
+            welcomeLabel.text = "Good Morning Sandie!"
+        }
+        else if(hour < 17){
+            welcomeLabel.text = "Good Afternoon Sandie!"
+        }
+        else{
+            welcomeLabel.text = "Good Evening Sandie!"
+        }
     }
     
     @objc func tick() {
